baa17e87a444bd155f690f43631445e3f794983c
CAM603 <cameronhawley@comcast.net>
2019-11-25 13:40:11 -0500

    added form

--- js/index.js
+++ js/index.js
@@ -4,7 +4,32 @@
 let btn = document.querySelectorAll('.btn');
 
 btn.forEach(el => {
-    el.addEventListener('mouseover', event => event.target.style.backgroundColor = '#BADA22');
+    el.addEventListener('mouseover', event => event.target.style.backgroundColor = 'lightcyan');
     el.addEventListener('mouseleave', event => event.target.style.backgroundColor = '');
 }); 
-    
+
+// Have form appear when clicking sign up buttons
+
+btn.forEach(el => {
+    el.addEventListener('click', event => {
+        let form = document.createElement('form');
+        form.setAttribute('id', 'myForm');
+        event.target.appendChild(form);
+
+
+        let input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.setAttribute('value', 'Your name here');
+        form.appendChild(input);
+        
+        
+        event.target.insertAdjacentElement('afterend', form)
+        
+    })
+})
+
+
+
+
+
+
