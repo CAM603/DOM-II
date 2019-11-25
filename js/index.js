8a52cc8c2511457bddb4f2cc9db752ec26721175
CAM603 <cameronhawley@comcast.net>
2019-11-25 12:16:26 -0500

    add btn mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+// Change text color of buttons with mouseover and mouseleave
+let btn = document.querySelectorAll('.btn');
+
+btn.forEach(el => {
+    el.addEventListener('mouseover', event => event.target.style.backgroundColor = '#BADA22');
+    el.addEventListener('mouseleave', event => event.target.style.backgroundColor = '');
+}); 
+    
